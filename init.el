d22bdd36858f25f2a7d5a4d26c096c671e39fa80
mattmc3 <mattmc3@gmail.com>
2021-04-24 10:21:09 -0400

    Checkpoint

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
 (defun my/reload-config ()
   "Reloads emacs configs"
   (interactive)
-  (org-babel-load-file (expand-file-name "myconfig.org" user-emacs-directory))
-  (org-babel-load-file (expand-file-name "subliminal.org" user-emacs-directory)))
+  (org-babel-load-file (expand-file-name "package-management.org" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "subliminal.org" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "myconfig.org" user-emacs-directory)))
 
 (my/reload-config)
 (global-set-key (kbd "s-R") 'my/reload-config)
---
+++ package-management.el
@@ -0,0 +1,16 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/") t)
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
