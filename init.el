7a4bac4db70ab72e43f4fe456e9d270e0bb1ea4a
mattmc3 <mattmc3@gmail.com>
2021-04-27 10:09:15 -0400

    Checkpoint

--- init.el
+++ init.el
@@ -1,9 +1,11 @@
 (defun my/reload-config ()
   "Reloads emacs configs"
   (interactive)
-  ;;(org-babel-load-file (expand-file-name "package-management.org" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "packages.org" user-emacs-directory))
   (org-babel-load-file (expand-file-name "subliminal.org" user-emacs-directory))
-  (org-babel-load-file (expand-file-name "myconfig.org" user-emacs-directory)))
+  (org-babel-load-file (expand-file-name "myconfig.org" user-emacs-directory))
+  ;; run subliminal
+  (subl/init))
 
 (my/reload-config)
 (global-set-key (kbd "s-R") 'my/reload-config)
