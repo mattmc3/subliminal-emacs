4cf7b0c10a3e9ec396a7024c889e3525449b1b02
mattmc3 <mattmc3@gmail.com>
2021-11-02 18:29:32 -0400

    Remove unnecessary files

--- init.el
+++
@@ -1,8 +0,0 @@
-(defun my/reload-config ()
-  "Reloads emacs configs"
-  (interactive)
-  (org-babel-load-file "~/.config/emacs/myconfig.org")
-  (org-babel-load-file "~/.config/emacs/subliminal.org"))
-
-(my/reload-config)
-(global-set-key (kbd "s-R") 'my/reload-config)
