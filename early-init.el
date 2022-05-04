b45b1fa7ff89bcae8270f0c9a11a8b618cf47cb0
mattmc3 <mattmc3@gmail.com>
2022-05-04 09:57:35 -0400

    Refactor early-init

--- early-init.el
+++ early-init.el
@@ -1,5 +1,15 @@
 ;;; early-init.el --- -*- lexical-binding: t -*-
 
+;;; Commentary
+
+;; References
+;; - https://github.com/hlissner/doom-emacs/blob/master/early-init.el
+;; - https://github.com/seagle0128/.emacs.d/blob/master/early-init.el
+;; - https://github.com/purcell/emacs.d/blob/master/early-init.el
+;; - https://github.com/SystemCrafters/rational-emacs/blob/master/early-init.el
+
+;;; Code
+
 ;; Documentation: (info "(emacs) Early Init File")
 ;;                (info "(emacs) Package Installation")
 
@@ -9,18 +19,7 @@
 ;; `package-load-list' like this:
 ;; (setq package-load-list '((org-journal nil) all))
 (setq package-enable-at-startup nil)
-
-;; Set these settings before the GUI frame is created.
-
-;; References
-;; - https://github.com/hlissner/doom-emacs/blob/master/early-init.el
-;; - https://github.com/seagle0128/.emacs.d/blob/master/early-init.el
-;; - https://github.com/purcell/emacs.d/blob/master/early-init.el
-
-(setq tool-bar-mode nil
-      menu-bar-mode t)
-
-;;(scroll-bar-mode 0)
+;; (setq package-quickstart t)
 
 ;; Switch off garbage collection (will be switched on later).
 ;; This is step 1 of 2. Step 2 is in init.
@@ -28,33 +27,36 @@
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-(setq package-quickstart t)
-
 ;; Premature redisplays can substantially affect startup times and produce
 ;; ugly flashes of unstyled Emacs.
-(setq-default inhibit-redisplay t
-              inhibit-message t)
-(add-hook 'window-setup-hook
-      (lambda ()
-            (setq-default inhibit-redisplay nil
-                          inhibit-message nil)
-            (redisplay)))
+;; (setq-default inhibit-redisplay t
+;;               inhibit-message t)
+;; (add-hook 'window-setup-hook
+;;       (lambda ()
+;;             (setq-default inhibit-redisplay nil
+;;                           inhibit-message nil)
+;;             (redisplay)))
 
 ;; Inhibit resizing frame
 (setq frame-inhibit-implied-resize t)
-
-;; Faster to disable these here (before they've been initialized)
-;;(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 ;; Contrary to what many Emacs users have in their configs, you don't need
 ;; more than this to make UTF-8 the default coding system:
 (set-language-environment "UTF-8")
 
-;; set-language-enviornment sets default-input-method, which is unwanted
+;; set-language-environment sets default-input-method, which is unwanted
 (setq default-input-method nil)
+
+;; set the initial window position an UI elements
+(setq default-frame-alist
+      '((height . 50)
+        (width . 150)
+        (left . 150)
+        (top . 150)
+        (vertical-scroll-bars . nil)
+        (horizontal-scroll-bars . nil)
+        (tool-bar-lines . 0)))
+
+(setq inhibit-startup-message t)
 
 ;; end early-init.el
